56fcd777cb5081d3c958c404f2bfa5e00579d48a
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-02 12:24:09 -0500

    Try to get react working through deployment

--- lib/stonekiller_web/controllers/page_controller.ex
+++ lib/stonekiller_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule StonekillerWeb.PageController do
   use StonekillerWeb, :controller
 
   def index(conn, _params) do
-    html(conn, File.read!("priv/static/index.html"))
+    html(conn, File.read!("./priv/static/index.html"))
   end
 end
