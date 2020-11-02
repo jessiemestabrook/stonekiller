93df40fb008e2b5266d453a19b588dbed8757a7b
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-02 15:16:11 -0500

    Fix this view

--- lib/stonekiller_web/views/page_view.ex
+++ lib/stonekiller_web/views/page_view.ex
@@ -2,7 +2,7 @@ defmodule StonekillerWeb.PageView do
   use StonekillerWeb, :view
 
   def render("pwa.html", _assigns) do
-    Path.join(:code.priv_dir(:your_app), "static/build/index.html")
+    Path.join(:code.priv_dir(:stonekiller), "static/build/index.html")
     |> File.read!()
     |> raw()
   end
