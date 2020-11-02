f0c28f5d5bdad5bdaad0bb9518b93c5dec2c112f
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-02 05:15:38 -0500

    Almost got elastic beanstalk working...

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,23 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :stonekiller, StonekillerWeb.Endpoint,
-  url: [host: "localhost", port: 4000],
+  http: [port: {:system, "PORT"}, compress: true],
+  url: [scheme: "http", host: System.get_env("HOST"), port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  code_reloader: false,
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
+
+config :stonekiller, Stonekiller.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: System.get_env("RDS_DB_NAME"),
+  username: System.get_env("RDS_USERNAME"),
+  password: System.get_env("RDS_PASSWORD"),
+  hostname: System.get_env("RDS_HOSTNAME"),
+  port: System.get_env("RDS_PORT") || 5432,
+  pool_size: 20,
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
