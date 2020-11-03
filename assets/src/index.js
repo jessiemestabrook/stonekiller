1843f55e5a3ef69b5cc97aa9f58eeea949748c95
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-03 04:31:12 -0500

    Start cleaning this up

--- assets/src/index.js
+++ assets/src/index.js
@@ -40,112 +40,90 @@ const requestAnimFrame = (function(){
 })();
 
 
-function nextNote() {
-  // Advance current note and time by a 16th note...
-  var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT
-                                        // tempo value to calculate beat length.
-  nextNoteTime += 0.25 * secondsPerBeat;    // Add beat length to last beat time
+// function nextNote() {
+//   // Advance current note and time by a 16th note...
+//   var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT
+//                                         // tempo value to calculate beat length.
+//   nextNoteTime += 0.25 * secondsPerBeat;    // Add beat length to last beat time
 
-  current16thNote++;    // Advance the beat number, wrap to zero
-  if (current16thNote == 16) {
-      current16thNote = 0;
-  }
-}
+//   current16thNote++;    // Advance the beat number, wrap to zero
+//   if (current16thNote == 16) {
+//       current16thNote = 0;
+//   }
+// }
 
-function scheduleNote( beatNumber, time ) {
-  // push the note on the queue, even if we're not playing.
-  notesInQueue.push( { note: beatNumber, time: time } );
+// function scheduleNote( beatNumber, time ) {
+//   // push the note on the queue, even if we're not playing.
+//   notesInQueue.push( { note: beatNumber, time: time } );
 
-  if ( (noteResolution==1) && (beatNumber%2))
-      return; // we're not playing non-8th 16th notes
-  if ( (noteResolution==2) && (beatNumber%4))
-      return; // we're not playing non-quarter 8th notes
+//   if ( (noteResolution==1) && (beatNumber%2))
+//       return; // we're not playing non-8th 16th notes
+//   if ( (noteResolution==2) && (beatNumber%4))
+//       return; // we're not playing non-quarter 8th notes
 
-  // create an oscillator
-  var osc = audioContext.createOscillator();
-  osc.connect( audioContext.destination );
-  if (beatNumber % 16 === 0)    // beat 0 == high pitch
-      osc.frequency.value = 880.0;
-  else if (beatNumber % 4 === 0 )    // quarter notes = medium pitch
-      osc.frequency.value = 440.0;
-  else                        // other 16th notes = low pitch
-      osc.frequency.value = 220.0;
+//   // create an oscillator
+//   var osc = audioContext.createOscillator();
+//   osc.connect( audioContext.destination );
+//   if (beatNumber % 16 === 0)    // beat 0 == high pitch
+//       osc.frequency.value = 880.0;
+//   else if (beatNumber % 4 === 0 )    // quarter notes = medium pitch
+//       osc.frequency.value = 440.0;
+//   else                        // other 16th notes = low pitch
+//       osc.frequency.value = 220.0;
 
-  osc.start( time );
-  osc.stop( time + noteLength );
-}
+//   osc.start( time );
+//   osc.stop( time + noteLength );
+// }
 
-function scheduler() {
-  // while there are notes that will need to play before the next interval,
-  // schedule them and advance the pointer.
-  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
-      scheduleNote( current16thNote, nextNoteTime );
-      nextNote();
-  }
-}
+// function scheduler() {
+//   // while there are notes that will need to play before the next interval,
+//   // schedule them and advance the pointer.
+//   while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
+//       scheduleNote( current16thNote, nextNoteTime );
+//       nextNote();
+//   }
+// }
 
-function play() {
-  if (!unlocked) {
-    // play silent buffer to unlock the audio
-    var buffer = audioContext.createBuffer(1, 1, 22050);
-    var node = audioContext.createBufferSource();
-    node.buffer = buffer;
-    node.start(0);
-    unlocked = true;
-  }
+// function play() {
+//   if (!unlocked) {
+//     // play silent buffer to unlock the audio
+//     var buffer = audioContext.createBuffer(1, 1, 22050);
+//     var node = audioContext.createBufferSource();
+//     node.buffer = buffer;
+//     node.start(0);
+//     unlocked = true;
+//   }
 
-  isPlaying = !isPlaying;
+//   isPlaying = !isPlaying;
 
-  if (isPlaying) { // start playing
-      current16thNote = 0;
-      nextNoteTime = audioContext.currentTime;
-      timerWorker.postMessage("start");
-  } else {
-      timerWorker.postMessage("stop");
-  }
-}
-
+//   if (isPlaying) { // start playing
+//       current16thNote = 0;
+//       nextNoteTime = audioContext.currentTime;
+//       timerWorker.postMessage("start");
+//   } else {
+//       timerWorker.postMessage("stop");
+//   }
+// }
 
 
 const audioContext = new AudioContext();
 
+
 // Set up the timer worker, which helps provide stability to the metronome
 timerWorker.onmessage = function(e) {
     if (e.data === "tick") {
-        scheduler();
+        // scheduler();
     } else {
       console.log("message: " + e.data);
     }
 };
 timerWorker.postMessage({"interval":lookahead});
-timerWorker.postMessage("start");
 
 
-function playBuffer(audioBuffer) {
-  const source = audioContext.createBufferSource();
-  source.buffer = audioBuffer;
-  source.connect(audioContext.destination);
-  source.start();
-}
-
-const clickAccentURL = 'https://metronome-audio-bucket.s3.amazonaws.com/click-accent.mp3';
-const clickUnaccentURL = 'https://metronome-audio-bucket.s3.amazonaws.com/click-unaccent.mp3'
-
-
-let clickAccentBuffer, clickUnaccentBuffer;
-
-window.fetch(clickAccentURL)
-  .then(response => response.arrayBuffer())
-  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-  .then(audioBuffer => {
-    // playButton.disabled = false;
-    clickAccentBuffer = audioBuffer;
-    playBuffer(clickAccentBuffer);
-  });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App audioContext={audioContext}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
