53f968a8e1ff1c605f878ea8bbcce7b118fae650
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-03 04:30:49 -0500

    Some styled components to start getting the UI together

---
+++ assets/src/components/styled.js
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+
+export const AppWrapper = styled.div`
+  background: #f7f7f7;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InterfaceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 360px;
+  text-align: center;
+`;
+
+export const StartButton = styled.button`
+  font-size: 3rem;
+  padding: 25px;
+  cursor: pointer;
+  margin-top: 40px;
+  background: #e6fff0;
+  border-color: #eee;
+  font-family: 'Roboto Mono', monospace;
+  user-select: none;
+`;
+
+export const ClockWrapper = styled.div`
+  text-align: center;
+  font-size: 4rem;
+  padding: 40px;
+
+  font-family: 'Roboto Mono', monospace;
+  font-weight: 100;
+  background: #e6f1ff;
+  user-select: none;
+`;
+
+export const BpmWrapper = styled.div`
+  padding: 40px;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 4rem;
+  background: #fff0f4;
+  user-select: none;
+`;
