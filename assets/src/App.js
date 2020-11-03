6ad92c50a03495c271acf6a3e149ee75431e2c07
patrickmestabrook <patrick.m.estabrook@gmail.com>
2020-11-03 04:31:00 -0500

    Start getting a UI together

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,34 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import {
+  AppWrapper,
+  InterfaceWrapper,
+  StartButton,
+  ClockWrapper,
+  BpmWrapper
+} from './components/styled';
 
-function App() {
+const clickAccentURL = 'https://metronome-audio-bucket.s3.amazonaws.com/click-accent.mp3';
+const clickUnaccentURL = 'https://metronome-audio-bucket.s3.amazonaws.com/click-unaccent.mp3'
+
+
+
+function App({ audioContext }) {
 
   // App has three overall states -- "begin", "workout", and "done"
   const [appState, setAppState] = useState('start');
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const [timeRemaining, setTimeRemaining] = useState("08:00");
+
+  const [tempo, setTempo] = useState(112);
+
+  function playBuffer(audioBuffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(audioContext.destination);
+    source.start();
+  }
+
+
+
+  useEffect(
+    () => {
+      let clickAccentBuffer, clickUnaccentBuffer;
+
+      window.fetch(clickAccentURL)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+        .then(audioBuffer => {
+          // playButton.disabled = false;
+          clickAccentBuffer = audioBuffer;
+          // playBuffer(clickAccentBuffer);
+        });
+    },
+    [], // just run on mount/unmount
+  );
 
   // const [tempo, setTempo] = useState(120);
 
   // const [current16thNote, setCurrent16thNote] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Ready to Practice</h2>
-        <button
-          style={{
-            padding: '20px',
-            fontSize: '2rem',
-            borderRadius: '23px'
-          }}
-
-          onClick={() => {
-            if (isPlaying) { setAppState("playing"); }
-            else { setAppState("playing")}
-          }}
-        >{isPlaying ? "stop" : "start"}</button>
-      </header>
-    </div>
+    <AppWrapper>
+      <InterfaceWrapper>
+        <ClockWrapper>{ timeRemaining }</ClockWrapper>
+        <BpmWrapper>{ tempo } bpm</BpmWrapper>
+        <StartButton>begin</StartButton>
+      </InterfaceWrapper>
+    </AppWrapper>
   );
 }
 
